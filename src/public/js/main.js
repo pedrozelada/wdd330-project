468827d0f3f1241972e4f63e9233cf15db70564d
pedrozelada <przs.gideon@gmail.com>
2024-12-16 11:04:05 -0400

    bug1.2

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -15,9 +15,9 @@ async function loadTemplate(path) {
 }
 
 async function loadHeaderFooter() {
-    const headerTemplate = await loadTemplate("./public/partials/header.html");
+    const headerTemplate = await loadTemplate("/partials/header.html");
     const headerElement = document.querySelector("#main-header");
-    const footerTemplate = await loadTemplate("./public/partials/footer.html");
+    const footerTemplate = await loadTemplate("/partials/footer.html");
     const footerElement = document.querySelector("#main-footer");
   
     renderWithTemplate(headerTemplate, headerElement,null, hamButton);

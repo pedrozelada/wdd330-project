b130806f931ed63443b04c46c3b6a0887eff393e
pedrozelada <przs.gideon@gmail.com>
2024-12-16 00:00:23 -0400

    build

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,22 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   root: "src/",
   publicDir: "../public",
   server: {
     port: 3000,
+  },
+  build: {
+    outDir: "../dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        animal: resolve(__dirname, "src/pages/animal.html"),
+        endangered: resolve(__dirname, "src/pages/endangered.html"),
+        extint: resolve(__dirname, "src/pages/extinct.html"),
+        about: resolve(__dirname, "src/pages/about.html"),
+      },
+    },
   },
 });
